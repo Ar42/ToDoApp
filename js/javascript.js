080be6968ab2fbd551c5239132bf5b064b509dd7
Ar42 <43175960+Ar42@users.noreply.github.com>
2021-09-14 23:57:10 +0600

    add, update, delete is ok now

--- js/javascript.js
+++ js/javascript.js
@@ -7,6 +7,9 @@ addTask.addEventListener("click", function () {
   valueOfinput_id = input_id.value;
   let t = valueOfinput_id.trim();
   if (t.length != 0) {
+    document.getElementById("zeroSpaceAlert").innerHTML =
+      "*Data added successfully";
+    document.getElementById("zeroSpaceAlert").style.color = "green";
     let checkAvaivaleItems = localStorage.getItem("localtask");
     if (checkAvaivaleItems == null) {
       toDoObj = [];
@@ -16,6 +19,10 @@ addTask.addEventListener("click", function () {
     toDoObj.push(valueOfinput_id);
 
     localStorage.setItem("localtask", JSON.stringify(toDoObj));
+  } else {
+    document.getElementById("zeroSpaceAlert").innerHTML =
+      "Please write something!!!";
+    document.getElementById("zeroSpaceAlert").style.color = "red";
   }
   input_id.value = "";
   output();
@@ -37,7 +44,7 @@ function output() {
       <td>${ind + 1}</td>
       <td>${element}</td>
       <td><button type="button" onclick="edit(${ind})"><a href="#input_id"><i class="fas fa-edit"></i></a></button></td>
-      <td><i class="fas fa-trash"></i></td>
+      <td><button type="button" onclick="deleteOne(${ind})"><i class="fas fa-trash"></i></button></td>
     </tr>
     <tr></tr>
   </tbody>`;
@@ -65,12 +72,15 @@ saveAterEdit.addEventListener("click", function () {
   toDoObj[hidden_id] = input_id.value;
   let t = input_id.value.trim();
   toDoObj[hidden_id] = input_id.value;
+
   if (t.length != 0) {
     localStorage.setItem("localtask", JSON.stringify(toDoObj));
     input_id.value = "";
     document.getElementById("zeroSpaceAlert").innerHTML =
       "*Data updated successfully";
     document.getElementById("zeroSpaceAlert").style.color = "green";
+    document.getElementById("addTask").style.display = "block";
+    document.getElementById("saveTask").style.display = "none";
   } else {
     document.getElementById("zeroSpaceAlert").innerHTML =
       "*please write something!!!";
@@ -78,3 +88,15 @@ saveAterEdit.addEventListener("click", function () {
   }
   output();
 });
+
+//delete
+function deleteOne(ind) {
+  let checkAvaivaleItems = localStorage.getItem("localtask");
+  let toDoObj = JSON.parse(checkAvaivaleItems);
+
+  localStorage.setItem("localtask", JSON.stringify(toDoObj));
+  toDoObj.splice(ind, 1);
+  localStorage.setItem("localtask", JSON.stringify(toDoObj));
+
+  output();
+}
